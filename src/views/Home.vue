ac80f2b328a2f68068db88165a04a1a749b8247b
Thiago Dourado <thiagocfdourado@gmail.com>
2021-07-18 12:24:49 -0300

    Add vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,70 +1,72 @@
 <template>
   <div class="home">
-
     <div>
-      <img class="logo" src="../assets/logo-unime-p.png" alt="Unime logo">
+      <img class="logo" src="../assets/logo-unime-p.png" alt="Unime logo" />
     </div>
     <div>
-        <label for="buscador"><strong>Buscar pelo Estado(UF)</strong></label><br>
+      <label for="buscador"><h3>Buscar pelo Estado(UF)</h3></label><br />
 
-        <select name="buscador" id="buscador">
-        <option v-for="estado of estados" :key="estado.uid">{{ estado.state }}</option>
-        </select>
+      <v-select 
+        v-model="select"
+        :hint="`${estado.state}`"
+        :items="estado.state"
+        label="Estado"      
+        dense>
+        <!-- <option v-for="estado of estados" :key="estado.uid">
+          {{ estado.state }}
+        </option> -->
+      </v-select>
 
     </div>
     <div id="dados">
-        <strong>Numero de casos (Confirmados)</strong>
-        <input type="text-area" disabled value="">
-        <input type="text-area" disabled value="" id="teste">
+      <strong>Numero de casos confirmados</strong>
     </div>
 
     <div class="status">
-        <img src="../assets/status-covid.png" alt="Status Covid">
+      <img src="../assets/status-covid.png" alt="Status Covid" />
     </div>
   </div>
 </template>
 <script>
-
 export default {
   data() {
     return {
-      estados: []
-    }
+      estados: [],
+    };
   },
   mounted() {
-      this.buscarDados()
+    this.buscarDados();
   },
   methods: {
-
     async buscarDados() {
-      const endpoint = "https://covid19-brazil-api.vercel.app/api/report/v1"
-    //   const estadoSelect = document.querySelector('#buscador')
+      const endpoint = "https://covid19-brazil-api.vercel.app/api/report/v1";
+      //   const estadoSelect = document.querySelector('#buscador')
 
       await fetch(endpoint)
-        .then(response => response.json())
-        .then(json => {
-            this.estados = json.data
-            // estadoSelect.innerHTML = ""
-         
-            // this.estados.forEach (estado => {
-            //     estadoSelect.innerHTML += `<option value="${estado.uf}">${estado.state}</option>`
-            })
-        }
-    }
-  }
+        .then((response) => response.json())
+        .then((json) => {
+          this.estados = json.data;
+          // estadoSelect.innerHTML = ""
 
+          // this.estados.forEach (estado => {
+          //     estadoSelect.innerHTML += `<option value="${estado.uf}">${estado.state}</option>`
+        });
+    },
+  },
+};
 </script>
 
 <style>
-    .logo {
-        width: 200px;
-        margin-top: 30px;
-        margin-bottom: 10px;
-    }
-    #buscador {
-        margin-top: 10px;
-    }
-    .status {
-        margin-top: 30px;
-    }
+.home {
+  text-align: center;
+}
+.logo {
+  width: 200px;
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+.container {
+  text-align: center;
+}
+
 </style>
