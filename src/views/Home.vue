95d04c81c3489915fe6fdc60b4e61b0262f06f13
Sérgio Filho <fragasergio23@gmail.com>
2021-07-18 10:45:01 -0300

    implementação do select de estados

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <img src="../assets/logo-unime-p.png" alt="Unime logo">
+    </div>
+    <div>
+      <label for="buscador"><strong>Buscar pelo Estado(UF)</strong></label><br>
+      <select name="buscador" id="buscador">
+        <option v-for="estado of estados" :key="estado.uid">{{ estado.state }}</option>
+        </select>
+      <!-- <button @click="buscarDados">Carregar Estados</button> -->
+      
+    </div>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      estados: []
+    }
+  },
+  mounted() {
+      this.buscarDados()
+  },
+  methods: {
+
+    async buscarDados() {
+      const endpoint = "https://covid19-brazil-api.vercel.app/api/report/v1"
+    //   const estadoSelect = document.querySelector('#buscador')
+
+      await fetch(endpoint)
+        .then(response => response.json())
+        .then(json => {
+            this.estados = json.data
+            // estadoSelect.innerHTML = ""
+         
+            // this.estados.forEach (estado => {
+            //     estadoSelect.innerHTML += `<option value="${estado.uf}">${estado.state}</option>`
+            })
+        }
+    }
   }
-}
+
 </script>
